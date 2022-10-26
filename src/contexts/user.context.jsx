19b723e4eb0482152592c4386a67b932c984da36
goorekt <asadiali008@gmail.com>
2022-10-26 15:16:32 +0200

    uid error

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,19 +1,21 @@
 import { createContext, useEffect, useState } from "react";
-import { onAuthStateChangedListener,createUserDocumentFromAuth } from "../utils/firebase.utils";
+import {
+	onAuthStateChangedListener,
+	createUserDocumentFromAuth,
+} from "../utils/firebase.utils";
 
 export const UserContext = createContext({
 	setCurrentUser: () => null,
-	currentUser: null,
+	currentUser: { uid: null },
 });
 
 export const UserProvider = ({ children }) => {
-	const [currentUser,setCurrentUser]=useState(null);
-	
+	const [currentUser, setCurrentUser] = useState(null);
+
 	const value = { currentUser, setCurrentUser };
-	
+
 	useEffect(() => {
 		const unsubscribe = onAuthStateChangedListener((user) => {
-            
 			if (user) {
 				createUserDocumentFromAuth(user);
 			}
