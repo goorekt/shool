c1ef2ccbcf97d44bb69349d3d28e487e93811256
goorekt <asadiali008@gmail.com>
2022-10-26 15:32:12 +0200

    like button

--- src/components/post/post.styles.js
+++ src/components/post/post.styles.js
@@ -1,72 +1,69 @@
-import styled, {css} from 'styled-components';
-
+import styled, { css } from "styled-components";
 
 const dislike = css`
-background-color:#8aff7dba;
+	background-color: #8aff7dba;
 `;
 const like = css`
-background-color:#ff6e6eba
+	background-color: #ff6e6eba;
 `;
 
 export const PostContainer = styled.div`
-  
-  display: grid;
-  flex-direction: column;
-  background: #fff;
-  border-radius: 0.5em;
-  padding: 1em;
-  margin: 1em;
+	display: grid;
+	flex-direction: column;
+	background: #fff;
+	border-radius: 0.5em;
+	padding: 1em;
+	margin: 1em;
 `;
 
 export const LikesContainer = styled.div`
-  width: 100%;
-  color: #9a0000;
-  font-size: 0.85em;
-  display:flex;
-  justify-content:space-between;
-  margin-top:1em;
- 
-`
+	width: 100%;
+	color: #9a0000;
+	font-size: 0.85em;
+	display: flex;
+	justify-content: space-between;
+	margin-top: 1em;
+`;
 export const Photo = styled.img`
-  width: 100%;
+	width: 100%;
 `;
 export const LikeButton = styled.div`
-  cursor:pointer;
-  display: inline-block
-  padding:5px;
-  width: 3em;
-  border-radius:4px;
-  text-align: center;
-  ${({liked})=>liked ? dislike:like};
+	cursor: pointer;
+	
+	padding: 5px;
+	width: 3em;
+	border-radius: 4px;
+	text-align: center;
+	${({ liked }) => (liked ? dislike : like)};
 `;
 export const ImageContainer = styled.div`
-  width: 5em;
-  height: 5em;
-  object-fit: cover;
-  border-radius: 50%;
+	width: 5em;
+	height: 5em;
+	object-fit: cover;
+	border-radius: 50%;
 `;
 export const TitleContainer = styled.div`
-  font-weight: 700;
+	font-weight: 700;
 `;
-export const CardHeader=styled.div`
-display: flex;
-flex-direction: column;
-  margin-top: 0.5em;
-  margin-left: 1em;
-  font-size: 1em;
-`
+export const CardHeader = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-top: 0.5em;
+	margin-left: 1em;
+	font-size: 1em;
+`;
 export const BodyText = styled.div`
-  font-size: 1.5em;
-  margin-top: 0.5em;
-  padding-bottom: 1em;
+	font-size: 1.5em;
+	margin-top: 0.5em;
+	padding-bottom: 1em;
 `;
 export const AuthorName = styled.div`
-color: #999;
-  font-size: 0.85em;
-  color: #aaa;
+	color: #999;
+	font-size: 0.85em;
+	color: #aaa;
 `;
-export const TimeStamp=styled.div`
-    color: #aaa;
-  margin-top: 0.25em;
-  font-size: 0.8em;
-`
+export const TimeStamp = styled.div`
+	color: #aaa;
+	margin-top: 0.25em;
+	font-size: 0.8em;
+`;
